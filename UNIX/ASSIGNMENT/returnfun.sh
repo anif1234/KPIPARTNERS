7da7812776c31c21693057918ae0cfb4974ca1c4
anif1234 <94674753+anif1234@users.noreply.github.com>
2021-11-23 18:12:33 +0530

    Add files via upload

---
+++ UNIX/ASSIGNMENT/returnfun.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+fun () {
+	return "the values are"
+}
+res=$?
+echo $res
+
---
+++ UNIX/ASSIGNMENT/switch.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+word="mr.spy"
+case "$word" in "anif")
+	echo "word in anif";;
+"abble")
+	echo "word in abble";;
+"mr.spy")
+	echo "word in mr.spy";;
+esac
